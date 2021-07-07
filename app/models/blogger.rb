1b2e3cf5a9788710243b5a18a309257d33124052
Anh Dang <anhcdang@gmail.com>
2021-07-07 09:38:32 -0700

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,23 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    validates :name, uniqueness: true, presence: true
+    validates :age, numericality: { greater_than: 0 }
+    validates :bio, length: { minimum: 31 }
+
+    # returns total number of likes on all of a blogger's posts
+    def total_likes
+        # access all of blogger's posts
+        # get all those posts' likes
+        # add up those likes
+        self.posts.map { |post| post.likes }.sum
+    end
+
+    # returns a blogger's post with the most likes
+    def featured_post
+        # access all of blogger's posts
+        # compare likes to get the max
+        self.posts.max_by { |post| post.likes }
+    end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,20 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
 
+    # returns the 5 most recent posts written about a destination
+    def five_most_recent_posts
+        self.posts.order("created_at").last(5)
+    end
+
+    # returns a destination's post with the most likes
+    def featured_post
+        self.posts.max_by { |post| post.likes }
+    end
+
+    #returns average age of all unique bloggers who have written about this destination
+    def bloggers_avg_age
+        bloggers_ages = self.bloggers.uniq.map { |blogger| blogger.age }
+        bloggers_ages.inject{ |sum, el| sum + el }.to_f / bloggers_ages.size
+    end
 end
